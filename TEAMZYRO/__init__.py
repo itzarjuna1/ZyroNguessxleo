c1736a09423f4c7ed1390b3825ab1c1242796318
MrZyro <zyrosukuna@gmail.com>
2025-06-07 13:53:19 +0530

    Update __init__.py

--- TEAMZYRO/__init__.py
+++ TEAMZYRO/__init__.py
@@ -1,3 +1,4 @@
 from config import *
+from pyrogram import filters, Client
 
-app = Client("ddw", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+app = Client("ZyroNguess", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
